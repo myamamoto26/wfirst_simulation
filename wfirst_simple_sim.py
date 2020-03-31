8399bbd6a54c3b221f618f6de8ea641a07654910
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-03-31 11:41:05 -0400

    x

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -840,7 +840,7 @@ def main(argv):
 
 def sub(argv):
     num = 5000000
-    dirr='v1_5'
+    dirr='v1_4'
     a=fio.FITS(dirr+'_sim_0.fits')[-1].read() 
     b=fio.FITS(dirr+'_sim_1.fits')[-1].read()
     c=fio.FITS(dirr+'_sim_2.fits')[-1].read()
