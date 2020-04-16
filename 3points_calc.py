1a59dfc9db2c7c6ccefe6cb9999e3d6c9f6174b9
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-04-16 14:33:30 -0400

    take delta

--- 3points_calc.py
+++ 3points_calc.py
@@ -44,12 +44,12 @@ def plot_3points(num, dirr1, dirr2, dirr3, dirr4):
 
     print(e002, e005)
 
-    deltae1 = e005 - e002
-    deltae2 = e2005 - e2002
+    deltae1 = [e005[i]-e002[i] for i in range(3)]
+    deltae2 = [e2005[i]-e2002[i] for i in range(3)]
 
     fig, ax1 = plt.subplots(figsize=(8,6))
-    ax1.plot(g002, deltae1, label='g1')
-    ax1.plot(g2002, deltae2, label='g2')
+    ax1.scatter(g002, deltae1, label='g1')
+    ax1.scatter(g2002, deltae2, label='g2')
     ax1.hlines(y=0, xmin=-0.01, xmax=0.01, linestyles='dashed')
     """
     ax1.plot(g002, e002, marker='o', c='b', label='g1=+0.02')
