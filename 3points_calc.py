b80c76afbd24baa3d3c4905588109f1c41fb5b2d
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-04-22 23:34:18 -0400

    plot

--- 3points_calc.py
+++ 3points_calc.py
@@ -15,11 +15,11 @@ def readinfiles(dirr):
 
     return a, b, c, d, e
 
-def plot_3points(num, dirr1, dirr2, dirr3, dirr4):
+def plot_3points(num, dirr1, dirr2, dirr3, dirr4, dirr5, dirr6):
 
     unsheared1, sheared1p1, sheared1m1, sheared2p1, sheared2m1 = readinfiles(dirr1)
-    unsheared2, sheared1p2, sheared1m2, sheared2p2, sheared2m2 = readinfiles(dirr2)
-    unsheared3, sheared1p3, sheared1m3, sheared2p3, sheared2m3 = readinfiles(dirr3)
+    unsheared2, sheared1p2, sheared1m2, sheared2p2, sheared2m2 = readinfiles(dirr3)
+    unsheared3, sheared1p3, sheared1m3, sheared2p3, sheared2m3 = readinfiles(dirr2)
     unsheared4, sheared1p4, sheared1m4, sheared2p4, sheared2m4 = readinfiles(dirr4)
 
     mask1 = (sheared1p1['g1'] == -0.02)
@@ -137,10 +137,259 @@ def plot_3points(num, dirr1, dirr2, dirr3, dirr4):
     plt.savefig('metacal_3points_fit.png')
     """
 
+def residual_bias(res_tot, gal_num):
+    g = 0.01
+
+    new = res_tot[0]
+    new1p = res_tot[1]
+    new1m = res_tot[2]
+    new2p = res_tot[3]
+    new2m = res_tot[4]
+
+    print(len(new1p["e1"]))
+    #old = old[old['ra']!=0]
+    #new = new[new['ra']!=0]
+    #new1p = new1p[new1p['ra']!=0]
+    #new1m = new1m[new1m['ra']!=0]
+    #new2p = new2p[new2p['ra']!=0]
+    #new2m = new2m[new2m['ra']!=0]
+
+    R11 = (new1p["e1"] - new1m["e1"])/(2*g)
+    R22 = (new2p["e2"] - new2m["e2"])/(2*g)
+    R12 = (new2p["e1"] - new2m["e1"])/(2*g)
+    R21 = (new1p["e2"] - new1m["e2"])/(2*g)
+
+    avg_R11 = np.mean(R11)
+    avg_R22 = np.mean(R22)
+    avg_R12 = np.mean(R12)
+    avg_R21 = np.mean(R21)
+
+    g1 = new['e1']/avg_R11
+    g2 = new['e2']/avg_R22
+
+    ## some statistics
+    print("Mean shear response: ")
+    N=len(new1p['e1'])
+    print(N)
+    print("<R11> = "+str("%6.4f"% avg_R11)+"+-"+str("%6.4f"% (np.std(R11)/np.sqrt(N))))
+    print("<R22> = "+str("%6.4f"% avg_R22)+"+-"+str("%6.4f"% (np.std(R22)/np.sqrt(N))))
+    print("<R12> = "+str("%6.4f"% avg_R12)+"+-"+str("%6.4f"% (np.std(R12)/np.sqrt(N))))
+    print("<R21> = "+str("%6.4f"% avg_R21)+"+-"+str("%6.4f"% (np.std(R21)/np.sqrt(N))))
+
+
+    def func(x,m,b):
+      return (1+m)*x+b
+
+    #params2 = curve_fit(func,new['g1'],new['e1']/avg_R11,p0=(0.,0.))
+    params2 = curve_fit(func,new['g1'],new['e1']/avg_R11,p0=(0.,0.))
+    m5,b5=params2[0]
+    m5err,b5err=np.sqrt(np.diagonal(params2[1]))
+    #params2 = curve_fit(func,new['g2'],new['e2']/avg_R22,p0=(0.,0.))
+    params2 = curve_fit(func,new['g2'],new['e2']/avg_R22,p0=(0.,0.))
+    m6,b6=params2[0]
+    m6err,b6err=np.sqrt(np.diagonal(params2[1]))
+
+    print("before correction: ")
+    print("m1="+str("%6.4f"% m5)+"+-"+str("%6.4f"% m5err), "b1="+str("%6.6f"% b5)+"+-"+str("%6.6f"% b5err))
+    print("m2="+str("%6.4f"% m6)+"+-"+str("%6.4f"% m6err), "b2="+str("%6.6f"% b6)+"+-"+str("%6.6f"% b6err))
+
+    return R11, R22, R12, R21
+
+def residual_bias_correction(a, b, c, d, e, gal_num):
+    g = 0.01
+    new = a
+    new1p = b
+    new1m = c
+    new2p = d
+    new2m = e
+
+    R11, R22, R12, R21 = residual_bias([a,b,c,d,e], gal_num)
+
+    avg_R11 = np.mean(R11)
+    avg_R22 = np.mean(R22)
+
+    snr_binn = 10
+    snr_min = np.log(15) #np.min(new['hlr']) #np.log(15) #np.log(min(new['snr']))
+    snr_max = np.log(500) #np.max(new['hlr']) #np.log(max(new['snr']))
+    snr_binslist = [snr_min+(x*((snr_max-snr_min)/10)) for x in range(11)]
+    #print(snr_min, snr_max, snr_binslist)
+    if snr_binslist[10] != snr_max:
+        print("raise an error.")
+
+    R11_g = []
+    R22_g = []
+    R12_g = []
+    R21_g = []
+    R11_gerr = []
+    R22_gerr = []
+    R12_gerr = []
+    R21_gerr = []
+    for a in range(10):
+        bin_R11 = []
+        bin_R22 = []
+        bin_R12 = []
+        bin_R21 = []
+        for b in range(len(R11)):
+            if (np.log(new['snr'][b]) >= snr_binslist[a]) and (np.log(new['snr'][b]) < snr_binslist[a+1]):
+            #if (new['hlr'][b] >= snr_binslist[a]) and (new['hlr'][b] < snr_binslist[a+1]):
+                bin_R11 += [R11[b]]
+                bin_R22 += [R22[b]]
+                bin_R12 += [R12[b]]
+                bin_R21 += [R21[b]]
+        #print(len(bin_R11))
+        R11_g += [np.mean(bin_R11)]
+        R22_g += [np.mean(bin_R22)]
+        R12_g += [np.mean(bin_R12)]
+        R21_g += [np.mean(bin_R21)]
+        R11_gerr += [np.std(bin_R11)/np.sqrt(len(bin_R11))]
+        R22_gerr += [np.std(bin_R22)/np.sqrt(len(bin_R22))]
+        R12_gerr += [np.std(bin_R12)/np.sqrt(len(bin_R12))]
+        R21_gerr += [np.std(bin_R21)/np.sqrt(len(bin_R21))]
+
+    ## getting cuts on the snr from the sheared catalogs and calculating selection response <R>selection
+    R11_s = []
+    R22_s = []
+    R12_s = []
+    R21_s = []
+    R11_serr = []
+    R22_serr = []
+    R12_serr = []
+    R21_serr = []
+    for i in range(10):
+        mask_1p = (np.log(new1p['snr']) >= snr_binslist[i]) & (np.log(new1p['snr']) < snr_binslist[i+1])
+        mask_1m = (np.log(new1m['snr']) >= snr_binslist[i]) & (np.log(new1m['snr']) < snr_binslist[i+1])
+        mask_2p = (np.log(new2p['snr']) >= snr_binslist[i]) & (np.log(new2p['snr']) < snr_binslist[i+1])
+        mask_2m = (np.log(new2m['snr']) >= snr_binslist[i]) & (np.log(new2m['snr']) < snr_binslist[i+1])
+        
+        #mask_1p = (new1p['hlr'] >= snr_binslist[i]) & (new1p['hlr'] < snr_binslist[i+1])
+        #mask_1m = (new1m['hlr'] >= snr_binslist[i]) & (new1m['hlr'] < snr_binslist[i+1])
+        #mask_2p = (new2p['hlr'] >= snr_binslist[i]) & (new2p['hlr'] < snr_binslist[i+1])
+        #mask_2m = (new2m['hlr'] >= snr_binslist[i]) & (new2m['hlr'] < snr_binslist[i+1])
+            
+        #print("how many objects fall in each bin. ", len(mask_1p), len(mask_1m), len(mask_2p), len(mask_2m))
+        
+        R11_s += [(np.mean(new['e1'][mask_1p]) - np.mean(new['e1'][mask_1m]))/(2*g)]
+        R22_s += [(np.mean(new['e2'][mask_2p]) - np.mean(new['e2'][mask_2m]))/(2*g)]
+        R12_s += [(np.mean(new['e1'][mask_2p]) - np.mean(new['e1'][mask_2m]))/(2*g)]
+        R21_s += [(np.mean(new['e2'][mask_1p]) - np.mean(new['e2'][mask_1m]))/(2*g)]
+
+    #print("to check if there is no nan or inf", R11_s, R11_g)
+    #print(R11_s)
+    if len(R11_s) != 10:
+        print('it is not 10 bins!')
+
+    ## total response
+    tot_R11 = []
+    tot_R22 = []
+    tot_R12 = []
+    tot_R21 = []
+    for k in range(10):
+        tot_R11 += [R11_s[k] + R11_g[k]]
+        tot_R22 += [R22_s[k] + R22_g[k]]
+        tot_R12 += [R12_s[k] + R12_g[k]]
+        tot_R21 += [R21_s[k] + R21_g[k]]
+        
+        
+    ## get the m&b values for each bin
+    from scipy.optimize import curve_fit
+    def func(x,m,b):
+      return (1+m)*x+b
+    m1_val = []
+    m1_err = []
+    b1_val = []
+    b1_err = []
+    m2_val = []
+    m2_err = []
+    b2_val =[]
+    b2_err = []
+    m3_val = []
+    m3_err = []
+    b3_val = []
+    b3_err = []
+    m4_val = []
+    m4_err = []
+    b4_val =[]
+    b4_err = []
+    for p in range(10):
+        mask = (np.log(new['snr']) >= snr_binslist[p]) & (np.log(new['snr']) < snr_binslist[p+1])
+        #mask = (new['hlr'] >= snr_binslist[p]) & (new['hlr'] < snr_binslist[p+1])
+
+        params = curve_fit(func,new['g1'][mask],new['e1'][mask]/tot_R11[p],p0=(0.,0.))
+        m1,b1=params[0]
+        m1err,b1err=np.sqrt(np.diagonal(params[1]))
+        params = curve_fit(func,new['g2'][mask],new['e2'][mask]/tot_R22[p],p0=(0.,0.))
+        m2,b2=params[0]
+        m2err,b2err=np.sqrt(np.diagonal(params[1]))
+        
+        params = curve_fit(func,new['g1'][mask],new['e1'][mask]/R11_g[p],p0=(0.,0.))
+        m3,b3=params[0]
+        m3err,b3err=np.sqrt(np.diagonal(params[1]))
+        params = curve_fit(func,new['g2'][mask],new['e2'][mask]/R22_g[p],p0=(0.,0.))
+        m4,b4=params[0]
+        m4err,b4err=np.sqrt(np.diagonal(params[1]))
+        
+        # corrected
+        m1_val += [m1]
+        m1_err += [m1err]
+        b1_val += [b1]
+        b1_err += [b1err]
+        m2_val += [m2]
+        m2_err += [m2err]
+        b2_val += [b2]
+        b2_err += [b2err]
+        
+        # not corrected
+        m3_val += [m3]
+        m3_err += [m3err]
+        b3_val += [b3]
+        b3_err += [b3err]
+        m4_val += [m4]
+        m4_err += [m4err]
+        b4_val += [b4]
+        b4_err += [b4err]
+
+    print(m1,b1)
+    print(m1_val, b1_val)
+    print('corrected m, b: ')
+    print("m1="+str("%6.4f"% np.mean(m1_val))+"+-"+str("%6.4f"% np.mean(m1_err)), "b1="+str("%6.6f"% np.mean(b1_val))+"+-"+str("%6.6f"% np.mean(b1_err)))
+    print("m2="+str("%6.4f"% np.mean(m2_val))+"+-"+str("%6.4f"% np.mean(m2_err)), "b2="+str("%6.6f"% np.mean(b2_val))+"+-"+str("%6.6f"% np.mean(b2_err)))
+
+    values=[m1_val,b1_val,m2_val,b2_val,m3_val,b3_val,m4_val,b4_val]
+    errors=[m1_err,b1_err,m2_err,b2_err,m3_err,b3_err,m4_err,b4_err]
+    return values, errors
+
+def plot_biasvsg(dir1, dir2, dir3, dir4, dir5, dir6):
+    values1, errors1 = residual_bias_correction(readinfiles(dirr1))
+    values2, errors2 = residual_bias_correction(readinfiles(dirr2))
+    values3, errors3 = residual_bias_correction(readinfiles(dirr3))
+    values4, errors4 = residual_bias_correction(readinfiles(dirr4))
+    values5, errors5 = residual_bias_correction(readinfiles(dirr5))
+    values6, errors6 = residual_bias_correction(readinfiles(dirr6))
+
+    app_shear = [-0.1, -0.05, -0.02, 0.02, 0.05, 0.1]
+    m1bias = [np.mean(values1[0]), np.mean(values3[0]), [np.mean(values5[0])]
+    m1biaserr = [np.mean(errors1[0]), np.mean(errors3[0]), np.mean(errors5[0])]
+    m2bias = [np.mean(values2[2]), np.mean(values4[2]), [np.mean(values6[2])]
+    m2biaserr = [np.mean(errors2[2]), np.mean(errors4[2]), np.mean(errors6[2])]
+
+    fig, ax1 = plt.subplots(figsize=(8,6))
+    ax1.scatter(app_shear, m1bias, label='m1')
+    ax1.errorbar(app_shear, m1bias, yerr=m1biaserr, format='o')
+    ax1.scatter(app_shear, m2bias, label='m2')
+    ax1.errorbar(app_shear, m2bias, yerr=m2biaserr, format='o')
+    ax1.set_xlabel('Applied Shear, g', fontsize=15)
+    ax1.set_ylabel('Multicative Bias, m', fontsize=15)
+    plt.legend(fontsize=14)
+    ax1.tick_params(labelsize=10)
+    plt.savefig('metacal_bias_shear.png')
+
 
 num = 5000000
 dirr1='v1_6' # g1=+-0.02
-dirr2='v1_10' # g1=+-0.05
-dirr3='v1_7'
+dirr2='v1_7' # g1=+-0.05
+dirr3='v1_10'
 dirr4='v1_11'
-plot_3points(num, dirr1, dirr2, dirr3, dirr4)
+dirr5='v1_13'
+dirr6='v1_14'
+#plot_3points(num, dirr1, dirr2, dirr3, dirr4)
+plot_biasvsg(dirr1, dirr2, dirr3, dirr4, dirr5, dirr6)
