8e289fc8dc9ba9980855e98e8f0c6cc759132ab0
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-05-12 11:18:31 -0400

    chromatic psf test

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -674,7 +674,7 @@ def main(argv):
     PSF_model = 'wfirst'
     stamp_size = 32
     hlr = 1.0
-    gal_num = 10
+    gal_num = 1000000
     bpass = wfirst.getBandpasses(AB_zeropoint=True)[filter_]
     galaxy_sed_n = galsim.SED('Mrk_33_spec.dat',  wave_type='Ang', flux_type='flambda')
 
@@ -751,16 +751,14 @@ def main(argv):
 
         gal_model = gal_model * galsim.wfirst.collecting_area * galsim.wfirst.exptime
         #gal_model = galsim.Convolve(gal_model, PSF)
-        print(gal_model)
+
         flux_ = gal_model.calculateFlux(bpass)
         #mag_ = gal_model.calculateMagnitude(bpass)
         # This makes the object achromatic, which speeds up drawing and convolution
         gal_model  = gal_model.evaluateAtWavelength(bpass.effective_wavelength)
         # Reassign correct flux
         gal_model  = gal_model.withFlux(flux_)
-        print(gal_model)
         gal_model = galsim.Convolve(gal_model, PSF)
-        print(gal_model)
 
         stamp_size_factor = old_div(int(gal_model.getGoodImageSize(wfirst.pixel_scale)), stamp_size)
         if stamp_size_factor == 0:
@@ -822,8 +820,7 @@ def main(argv):
         #gal_stamp.write(str(i_gal)+'.fits')
 
         res_tot = get_coadd_shape(cat, gal_stamp, psf_stamp, sky_image, i_gal, hlr, res_tot, g1, g2)
-    print(time.time()-t0)
-    exit()
+    
     ## send and receive objects from one processors to others
     if rank!=0:
         # send res_tot to rank 0 processor
