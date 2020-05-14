a631a98e2db3415eeda1e75ae9aa17edff86b477
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-05-14 14:05:08 -0400

    rotational dithering check ds9

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -295,7 +295,7 @@ def get_exp_list(gal, psf, thetas, offsets, sky_stamp, psf2=None):
             dvdcol=jacob.dvdx*np.cos(thetas[i]) - jacob.dudx*np.sin(thetas[i]),
             dudrow=jacob.dudy*np.cos(thetas[i]) + jacob.dvdy*np.sin(thetas[i]),
             dudcol=jacob.dudx*np.cos(thetas[i]) + jacob.dvdx*np.sin(thetas[i]))
-        #print(gal_jacob)
+        print(gal_jacob)
         psf_jacob2 = gal_jacob
 
         mask = np.where(weight!=0)
@@ -837,9 +837,9 @@ def main(argv):
             #print(hsm(gal_stamp, psf=psf_stamp, wt=sky_image.invertSelf()))
 
             gal_stamp.write(str(i)+'_rotate.fits')
-        exit()
         #res_tot = get_coadd_shape(cat, gals, psfs, thetas, offsets, i_gal, hlr, res_tot, g1, g2)
         res_tot = get_coadd_shape(cat, gals, psfs, thetas, offsets, skys, i_gal, hlr, res_tot, g1, g2)
+        exit()
     
     ## send and receive objects from one processors to others
     if rank!=0:
