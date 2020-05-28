2ff3cc10ebb91bded74899a09447a62ff77da853
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-05-28 15:02:08 -0400

    rotational dithers

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -827,7 +827,7 @@ def main(argv):
         ## translational dither check (multiple exposures)
         random_dir = galsim.UniformDeviate(rng)
         wcs=[wcs1,wcs2]
-        print(wcs1.toWorld(image_pos=(galsim.PositionI(2048,2048))))
+        print(wcs1.toWorld(galsim.PositionI(old_div(wfirst.n_pix,2),old_div(wfirst.n_pix,2))))
         offsets = []
         thetas = [position_angle1*(np.pi/180)*galsim.radians, position_angle2*(np.pi/180)*galsim.radians]
         gals = []
