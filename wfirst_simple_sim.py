fcd7e3a10208003819ca4f9a2429acd9763414e3
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-06-05 18:40:39 -0400

    rotational dithers run

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -862,7 +862,6 @@ def main(argv):
             origin_x = gal_stamp.origin.x
             origin_y = gal_stamp.origin.y
             gal_stamp.setOrigin(0,0)
-            psf_stamp.setOrigin(0,0)
             new_pos = galsim.PositionD(xy.x-origin_x, xy.y-origin_y)
             wcs_transf = gal_stamp.wcs.affine(image_pos=new_pos)
             new_wcs = galsim.JacobianWCS(wcs_transf.dudx, wcs_transf.dudy, wcs_transf.dvdx, wcs_transf.dvdy)
