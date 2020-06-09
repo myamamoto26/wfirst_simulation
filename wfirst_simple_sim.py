ade9304fd1424d70bfa3017687d32c2190e983b6
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-06-09 09:42:54 -0400

    check on m2, v2_8 correction

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -932,7 +932,7 @@ def sub(argv):
 
 if __name__ == "__main__":
 
-    
+    """
     t0 = time.time()
     
     comm = MPI.COMM_WORLD
@@ -946,8 +946,9 @@ if __name__ == "__main__":
     cat = fio.FITS('truth_mag.fits')[-1].read()
 
     main(sys.argv)
+    """
     
-    #sub(sys.argv)
+    sub(sys.argv)
 
 
 
