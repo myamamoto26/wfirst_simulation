2359164dca18c3573f7a826561356217026a5d5f
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-06-23 14:10:41 -0400

    offset 60

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -482,7 +482,7 @@ def main(argv):
     PSF = getPSF(PSF_model, use_SCA, filter_, bpass)
     
     position_angle1=20 #degrees
-    position_angle2=55 #degrees
+    position_angle2=80 #degrees
     wcs1, sky_level1 = get_wcs(dither_i, use_SCA, filter_, stamp_size, position_angle1)
     wcs2, sky_level2 = get_wcs(dither_i, use_SCA, filter_, stamp_size, position_angle2)
     wcs=[wcs1, wcs2]
@@ -670,7 +670,7 @@ def main(argv):
                     res_tot[j][col]+=res_[j][col]
 
     if rank==0:
-        dirr='v2_7_offset_35'
+        dirr='v2_7_offset_60'
         for i in range(len(res_tot)):
             fio.write(dirr+'_sim_'+str(i)+'.fits', res_tot[i])
             
