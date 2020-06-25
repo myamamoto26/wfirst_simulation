bb948c329f23cbb0d8eca980361ca3f171813a9b
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-06-25 18:04:13 -0400

    takes in 4 arguments

--- metacal_rot.py
+++ metacal_rot.py
@@ -24,6 +24,7 @@ from ngmix import priors, joint_prior
 import mof
 import meds
 
+import wfirst_simple_sim
 from wfirst_simple_sim import get_exp_list
 
 """
@@ -41,6 +42,8 @@ from wfirst_simple_sim import get_exp_list
 
 def Observation():
 	# Obtain observation list, psf list, weight from wfirst_simple_sim.py
+	gals=wfirst_simple_sim.main(1, 'Gaussian', "Gaussian", 'metacal')
+
 	gals=[fio.FITS('mcal_gal_0.fits')[-1].read(), fio.FITS('mcal_gal_1.fits')[-1].read()]
 	psfs=[fio.FITS('mcal_psf_0.fits')[-1].read(), fio.FITS('mcal_psf_1.fits')[-1].read()]
 	offsets=[galsim.PositionD(-0.5,-0.5), galsim.PositionD(0.23248147187155155,0.3059909382209298)]
