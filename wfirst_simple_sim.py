168277581751359f95f3c80cfd34e4ec22ca6917
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-06-26 20:19:00 -0400

    fixed angle 70

--- wfirst_simple_sim.py
+++ wfirst_simple_sim.py
@@ -483,7 +483,7 @@ def main(argv):
 
     PSF = getPSF(PSF_model, use_SCA, filter_, bpass)
     position_angle1=70 #degrees
-    position_angle2=115 #degrees
+    position_angle2=70 #degrees
     wcs1, sky_level1 = get_wcs(dither_i, use_SCA, filter_, stamp_size, position_angle1)
     wcs2, sky_level2 = get_wcs(dither_i, use_SCA, filter_, stamp_size, position_angle2)
     wcs=[wcs1, wcs2]
@@ -679,7 +679,7 @@ def main(argv):
                     res_tot[j][col]+=res_[j][col]
 
     if rank==0:
-        dirr='v2_7_fix70_offset_45'
+        dirr='v2_7_fix70_offset_0'
         for i in range(len(res_tot)):
             fio.write(dirr+'_sim_'+str(i)+'.fits', res_tot[i])
             
