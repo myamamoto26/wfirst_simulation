086d1a601c06ba890d98b99620350dcc9a2a59d1
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-11-06 14:29:22 -0500

    indent

--- des_analysis.py
+++ des_analysis.py
@@ -50,45 +50,44 @@ def analyze_g12(new,new1p,new1m,new2p,new2m):
 	avg_R21 = np.mean(R21)
 
 	print("Mean shear response: ")
-    N=len(new1p['e1'])
-    print(N)
-    print("<R11> = "+str("%6.4f"% avg_R11)+"+-"+str("%6.4f"% (np.std(R11)/np.sqrt(N))))
-    print("<R22> = "+str("%6.4f"% avg_R22)+"+-"+str("%6.4f"% (np.std(R22)/np.sqrt(N))))
-    print("<R12> = "+str("%6.4f"% avg_R12)+"+-"+str("%6.4f"% (np.std(R12)/np.sqrt(N))))
-    print("<R21> = "+str("%6.4f"% avg_R21)+"+-"+str("%6.4f"% (np.std(R21)/np.sqrt(N))))
+	N=len(new1p['e1'])
+	print(N)
+	print("<R11> = "+str("%6.4f"% avg_R11)+"+-"+str("%6.4f"% (np.std(R11)/np.sqrt(N))))
+	print("<R22> = "+str("%6.4f"% avg_R22)+"+-"+str("%6.4f"% (np.std(R22)/np.sqrt(N))))
+	print("<R12> = "+str("%6.4f"% avg_R12)+"+-"+str("%6.4f"% (np.std(R12)/np.sqrt(N))))
+	print("<R21> = "+str("%6.4f"% avg_R21)+"+-"+str("%6.4f"% (np.std(R21)/np.sqrt(N))))
 
-    def func(x,m,b):
-      return (1+m)*x+b
+	def func(x,m,b):
+    	return (1+m)*x+b
     def func_off(x,m,b):
-        return m*x+b
+		return m*x+b
 
-    gamma1_obs = new['e1']/avg_R11
-    params2 = curve_fit(func,new['g1'],gamma1_obs,p0=(0.,0.))
-    m5,b5=params2[0]
-    m5err,b5err=np.sqrt(np.diagonal(params2[1]))
+	gamma1_obs = new['e1']/avg_R11
+	params2 = curve_fit(func,new['g1'],gamma1_obs,p0=(0.,0.))
+	m5,b5=params2[0]
+	m5err,b5err=np.sqrt(np.diagonal(params2[1]))
 
-    gamma2_obs = new['e2']/avg_R22
-    params2 = curve_fit(func,new['g2'],gamma2_obs,p0=(0.,0.))
-    m6,b6=params2[0]
-    m6err,b6err=np.sqrt(np.diagonal(params2[1]))
+	gamma2_obs = new['e2']/avg_R22
+	params2 = curve_fit(func,new['g2'],gamma2_obs,p0=(0.,0.))
+	m6,b6=params2[0]
+	m6err,b6err=np.sqrt(np.diagonal(params2[1]))
 
-    ## off-diagonal bias check
-    params_off1 = curve_fit(func_off,new['g2'],gamma1_obs,p0=(0.,0.))
-    params_off2 = curve_fit(func_off,new['g1'],gamma2_obs,p0=(0.,0.))
-    m12, c12 = params_off1[0]
-    m12_err, c12_err = np.sqrt(np.diagonal(params_off1[1]))
-    m21, c21 = params_off2[0]
-    m21_err, c21_err = np.sqrt(np.diagonal(params_off2[1]))
+	## off-diagonal bias check
+	params_off1 = curve_fit(func_off,new['g2'],gamma1_obs,p0=(0.,0.))
+	params_off2 = curve_fit(func_off,new['g1'],gamma2_obs,p0=(0.,0.))
+	m12, c12 = params_off1[0]
+	m12_err, c12_err = np.sqrt(np.diagonal(params_off1[1]))
+	m21, c21 = params_off2[0]
+	m21_err, c21_err = np.sqrt(np.diagonal(params_off2[1]))
 
-    print('off-diagonal cpomponents: ')
-    print("m12="+str("%6.4f"% m12)+"+-"+str("%6.4f"% m12_err), "b12="+str("%6.6f"% c12)+"+-"+str("%6.6f"% c12_err))
-    print("m21="+str("%6.4f"% m21)+"+-"+str("%6.4f"% m21_err), "b21="+str("%6.6f"% c21)+"+-"+str("%6.6f"% c21_err))
+	print('off-diagonal cpomponents: ')
+	print("m12="+str("%6.4f"% m12)+"+-"+str("%6.4f"% m12_err), "b12="+str("%6.6f"% c12)+"+-"+str("%6.6f"% c12_err))
+	print("m21="+str("%6.4f"% m21)+"+-"+str("%6.4f"% m21_err), "b21="+str("%6.6f"% c21)+"+-"+str("%6.6f"% c21_err))
+	print("before correction: ")
+	print("m1="+str("%6.4f"% m5)+"+-"+str("%6.4f"% m5err), "b1="+str("%6.6f"% b5)+"+-"+str("%6.6f"% b5err))
+	print("m2="+str("%6.4f"% m6)+"+-"+str("%6.4f"% m6err), "b2="+str("%6.6f"% b6)+"+-"+str("%6.6f"% b6err))
 
-    print("before correction: ")
-    print("m1="+str("%6.4f"% m5)+"+-"+str("%6.4f"% m5err), "b1="+str("%6.6f"% b5)+"+-"+str("%6.6f"% b5err))
-    print("m2="+str("%6.4f"% m6)+"+-"+str("%6.4f"% m6err), "b2="+str("%6.6f"% b6)+"+-"+str("%6.6f"% b6err))
-
-    return R11, R22, R12, R21, gamma1_obs, gamma2_obs
+	return R11, R22, R12, R21, gamma1_obs, gamma2_obs
 
 def residual_bias_correction(new, new1p, new1m, new2p, new2m):
     g = 0.01
