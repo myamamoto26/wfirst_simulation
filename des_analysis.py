8bfc1712ead79f61513157fccb168f82fdea155c
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-11-06 11:23:32 -0500

    standard des analysis

--- des_analysis.py
+++ des_analysis.py
@@ -75,11 +75,11 @@ def main(argv):
         if j==0:
         	g1_true, g1_obs = analyze_g1(new,new1p,new1m,new2p,new2m)
         elif j==1:
-			g1n_true, g1n_obs = analyze_g1(new,new1p,new1m,new2p,new2m)
-		elif j==2:
-			g2_true, g2_obs = analyze_g2(new,new1p,new1m,new2p,new2m)
-		elif j==3:
-			g2n_true, g2n_obs = analyze_g2(new,new1p,new1m,new2p,new2m)
+        	g1n_true, g1n_obs = analyze_g1(new,new1p,new1m,new2p,new2m)
+        elif j==2:
+        	g2_true,g2_obs = analyze_g2(new,new1p,new1m,new2p,new2m)
+        elif j==3:
+        	g2n_true,g2n_obs = analyze_g2(new,new1p,new1m,new2p,new2m)
     
     def func(x,m,b):
     	return (1+m)*x+b
