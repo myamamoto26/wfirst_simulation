ec68e8c842b92d94a784b9fdcff21a2ccb97cc0d
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-11-06 12:09:36 -0500

    find the object ind

--- find.py
+++ find.py
@@ -4,7 +4,8 @@ import fitsio as fio
 a=fio.FITS('/hpc/group/cosmology/phy-lsst/my137/g1002/ngmix/fiducial_H158_mcal_noshear.fits')[-1].read()
 b=fio.FITS('/hpc/group/cosmology/phy-lsst/my137/g1n002/ngmix/fiducial_H158_mcal_noshear.fits')[-1].read()
 
-print(len(a['ind'], len(b['ind']))
+print(len(a['ind'], len(b['ind'])))
+
 for i in range(len(b['ind'])):
 	if b['ind'][i] not in a['ind']:
 		print(b['ind'][i])
