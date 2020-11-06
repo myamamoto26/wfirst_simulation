8d4a376727f1ab89ae59c438e8fd74b422f7e200
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-11-06 11:20:24 -0500

    standard des analysis

--- des_analysis.py
+++ des_analysis.py
@@ -73,7 +73,7 @@ def main(argv):
         start+=len(new_)
 
         if j==0:
-			g1_true, g1_obs = analyze_g1(new,new1p,new1m,new2p,new2m)
+        	g1_true, g1_obs = analyze_g1(new,new1p,new1m,new2p,new2m)
 		elif j==1:
 			g1n_true, g1n_obs = analyze_g1(new,new1p,new1m,new2p,new2m)
 		elif j==2:
