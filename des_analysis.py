d7bedae96f381327208440f4faf4b271ec55d76d
Masaya Yamamoto <masayamn@masaya-mac.home>
2020-12-07 16:50:38 -0500

    coadd false

--- des_analysis.py
+++ des_analysis.py
@@ -204,7 +204,7 @@ def main(argv):
 	old = None
 	f = sys.argv[2]
 	filter_ = sys.argv[3]
-	coadd_ = True
+	coadd_ = False
 
 	if not coadd_:
 		folder=[f+'/g1002/ngmix/',f+'/g1n002/ngmix/', f+'/g2002/ngmix/',f+'/g2n002/ngmix/']
