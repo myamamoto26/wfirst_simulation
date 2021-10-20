cacea7c6abe79fafe0fa1d86a1a11acfb42feaa8
Masaya Yamamoto <my137@duke.edu>
2021-10-20 15:31:17 -0400

    single vs coadd

--- mean_shear_nperbin.py
+++ mean_shear_nperbin.py
@@ -9,6 +9,7 @@ from matplotlib import pyplot as plt
 work = '/hpc/group/cosmology/phy-lsst/my137/roman_H158/'
 work_out = '/hpc/group/cosmology/masaya/wfirst_simulation/paper/'
 coadd_path = 'new_coadd_oversample'
+single_path = 'new_single'
 sims = ['g1002', 'g1n002', 'g2002', 'g2n002']
 
 def mean_shear_nperbin(new, new1p, new1m, new2p, new2m, nperbin, par):
@@ -34,81 +35,87 @@ shear1p = []
 shear1m = []
 shear2p = []
 shear2m = []
-for i in range(4): # four sets of sim. 
-    mcal_noshear = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+coadd_path+"/fiducial_H158_mcal_noshear.fits"))[-1].read()
-    mcal_1p = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+coadd_path+"/fiducial_H158_mcal_1p.fits"))[-1].read()
-    mcal_1m = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+coadd_path+"/fiducial_H158_mcal_1m.fits"))[-1].read()
-    mcal_2p = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+coadd_path+"/fiducial_H158_mcal_2p.fits"))[-1].read()
-    mcal_2m = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+coadd_path+"/fiducial_H158_mcal_2m.fits"))[-1].read()
-
-    mask = (mcal_noshear['flags']==0) & (mcal_noshear['ind']!=0)
-    noshear.append(mcal_noshear[mask])
-    shear1p.append(mcal_1p[mask])
-    shear1m.append(mcal_1m[mask])
-    shear2p.append(mcal_2p[mask])
-    shear2m.append(mcal_2m[mask])
-
-a,c00,c1 = np.intersect1d(noshear[0]['ind'], noshear[1]['ind'], return_indices=True)
-b,c01,c2 = np.intersect1d(noshear[0]['ind'][c00], noshear[2]['ind'], return_indices=True)
-c,c02,c3 = np.intersect1d(noshear[0]['ind'][c00][c01], noshear[3]['ind'], return_indices=True)
-tmp_ind = noshear[0]['ind'][c00][c01][c02]
-for run in range(1):
-    new = noshear[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
-    new1p = shear1p[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
-    new1m = shear1m[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
-    new2p = shear2p[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
-    new2m = shear2m[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
-
-    bin_mean_snr, g1_obs_snr, g1err_obs_snr = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, 'coadd_snr')
-    bin_mean_T, g1_obs_T, g1err_obs_T = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, 'coadd_hlr')
-    bin_mean_Tpsf, g1_obs_Tpsf, g1err_obs_Tpsf = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, 'coadd_psf_T')
-
-    gamma1_t,gamma2_t,gamma1_o,gamma2_o,noshear1,noshear2 = analyze_gamma_obs(new,new1p,new1m,new2p,new2m,coadd_=True)
-
 fig,axs = plt.subplots(1,3,figsize=(20,6),dpi=100,sharey=True)
+for p,j in enumerate([coadd_path, single_path]):
+    if p==0:
+        xax = ['coadd_snr', 'coadd_hlr', 'coadd_psf_T']
+        coadd_=True
+    elif p==1:
+        xax = ['snr', 'hlr', 'psf_T']
+        coadd_=False
+    for i in range(4): # four sets of sim. 
+        mcal_noshear = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+j+"/fiducial_H158_mcal_noshear.fits"))[-1].read()
+        mcal_1p = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+j+"/fiducial_H158_mcal_1p.fits"))[-1].read()
+        mcal_1m = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+j+"/fiducial_H158_mcal_1m.fits"))[-1].read()
+        mcal_2p = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+j+"/fiducial_H158_mcal_2p.fits"))[-1].read()
+        mcal_2m = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+j+"/fiducial_H158_mcal_2m.fits"))[-1].read()
 
-# d_x = [new['coadd_snr'], new['coadd_hlr'], new['coadd_psf_T']]
-# x_label = ['S/N', 'T', 'T_{psf}']
-# for q,ax in enumerate(axs.ravel()):
+        mask = (mcal_noshear['flags']==0) & (mcal_noshear['ind']!=0)
+        noshear.append(mcal_noshear[mask])
+        shear1p.append(mcal_1p[mask])
+        shear1m.append(mcal_1m[mask])
+        shear2p.append(mcal_2p[mask])
+        shear2m.append(mcal_2m[mask])
 
-#     x_ = d_x[q]
-#     hist = stat.histogram(x_, nperbin=50000, more=True)
-#     bin_num = len(hist['hist'])
-#     g_obs = np.zeros(bin_num)
-#     gerr_obs = np.zeros(bin_num)
-#     print(len(hist['low']), len(hist['mean']))
-#     for i in range(bin_num):
-#         bin_mask = (x_ > hist['low'][i]) & (x_ < hist['high'][i])
-#         g_obs[i] = np.mean(gamma1_o[bin_mask])
-#         gerr_obs[i] = np.std(gamma1_o[bin_mask])/np.sqrt(len(gamma1_o[bin_mask]))
+    a,c00,c1 = np.intersect1d(noshear[0]['ind'], noshear[1]['ind'], return_indices=True)
+    b,c01,c2 = np.intersect1d(noshear[0]['ind'][c00], noshear[2]['ind'], return_indices=True)
+    c,c02,c3 = np.intersect1d(noshear[0]['ind'][c00][c01], noshear[3]['ind'], return_indices=True)
+    tmp_ind = noshear[0]['ind'][c00][c01][c02]
+    for run in range(1):
+        new = noshear[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
+        new1p = shear1p[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
+        new1m = shear1m[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
+        new2p = shear2p[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
+        new2m = shear2m[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
 
-#     ax.hlines(0.02, 0, hist['mean'][len(hist['mean'])-1],linestyles='dashed')
-#     ax.errorbar(hist['mean'], g_obs, yerr=gerr_obs, fmt='o', fillstyle='none')
-#     ax.set_xlabel(x_label[q])
-#     ax.set_xscale('log')
-#     ax.set_ylabel('<e1>')
-#     ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        bin_mean_snr, g1_obs_snr, g1err_obs_snr = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[0])
+        bin_mean_T, g1_obs_T, g1err_obs_T = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[1])
+        bin_mean_Tpsf, g1_obs_Tpsf, g1err_obs_Tpsf = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[2])
+
+        gamma1_t,gamma2_t,gamma1_o,gamma2_o,noshear1,noshear2 = analyze_gamma_obs(new,new1p,new1m,new2p,new2m,coadd_=coadd_)
+
+    # d_x = [new['coadd_snr'], new['coadd_hlr'], new['coadd_psf_T']]
+    # x_label = ['S/N', 'T', 'T_{psf}']
+    # for q,ax in enumerate(axs.ravel()):
+
+    #     x_ = d_x[q]
+    #     hist = stat.histogram(x_, nperbin=50000, more=True)
+    #     bin_num = len(hist['hist'])
+    #     g_obs = np.zeros(bin_num)
+    #     gerr_obs = np.zeros(bin_num)
+    #     print(len(hist['low']), len(hist['mean']))
+    #     for i in range(bin_num):
+    #         bin_mask = (x_ > hist['low'][i]) & (x_ < hist['high'][i])
+    #         g_obs[i] = np.mean(gamma1_o[bin_mask])
+    #         gerr_obs[i] = np.std(gamma1_o[bin_mask])/np.sqrt(len(gamma1_o[bin_mask]))
+
+    #     ax.hlines(0.02, 0, hist['mean'][len(hist['mean'])-1],linestyles='dashed')
+    #     ax.errorbar(hist['mean'], g_obs, yerr=gerr_obs, fmt='o', fillstyle='none')
+    #     ax.set_xlabel(x_label[q])
+    #     ax.set_xscale('log')
+    #     ax.set_ylabel('<e1>')
+    #     ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
 
-axs[0].hlines(0.02, 0, bin_mean_snr[len(bin_mean_snr)-1],linestyles='dashed')
-axs[1].hlines(0.02, 0, bin_mean_T[len(bin_mean_T)-1],linestyles='dashed')
-axs[2].hlines(0.02, 0, bin_mean_Tpsf[len(bin_mean_Tpsf)-1],linestyles='dashed')
-axs[0].errorbar(bin_mean_snr, g1_obs_snr, yerr=g1err_obs_snr, fmt='o', fillstyle='none')
-axs[1].errorbar(bin_mean_T, g1_obs_T, yerr=g1err_obs_T, fmt='o', fillstyle='none')
-axs[2].errorbar(bin_mean_Tpsf, g1_obs_Tpsf, yerr=g1err_obs_Tpsf, fmt='o', fillstyle='none')
-axs[0].set_xlabel('S/N', fontsize=15)
-axs[1].set_xlabel('T', fontsize=15)
-axs[2].set_xlabel(r'$T_{psf}$', fontsize=15)
-axs[0].set_xscale('log')
-axs[1].set_xscale('log')
-axs[2].set_xscale('log')
-axs[0].set_ylabel(r'$<e_{1}>$', fontsize=15)
-axs[0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-axs[0].tick_params(labelsize=13)
-axs[1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-axs[1].tick_params(labelsize=13)
-axs[2].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-axs[2].tick_params(labelsize=13)
+    axs[0].hlines(0.02, 0, bin_mean_snr[len(bin_mean_snr)-1],linestyles='dashed')
+    axs[1].hlines(0.02, 0, bin_mean_T[len(bin_mean_T)-1],linestyles='dashed')
+    axs[2].hlines(0.02, 0, bin_mean_Tpsf[len(bin_mean_Tpsf)-1],linestyles='dashed')
+    axs[0].errorbar(bin_mean_snr, g1_obs_snr, yerr=g1err_obs_snr, fmt='o', fillstyle='none')
+    axs[1].errorbar(bin_mean_T, g1_obs_T, yerr=g1err_obs_T, fmt='o', fillstyle='none')
+    axs[2].errorbar(bin_mean_Tpsf, g1_obs_Tpsf, yerr=g1err_obs_Tpsf, fmt='o', fillstyle='none')
+    axs[0].set_xlabel('S/N', fontsize=15)
+    axs[1].set_xlabel('T', fontsize=15)
+    axs[2].set_xlabel(r'$T_{psf}$', fontsize=15)
+    axs[0].set_xscale('log')
+    axs[1].set_xscale('log')
+    axs[2].set_xscale('log')
+    axs[0].set_ylabel(r'$<e_{1}>$', fontsize=15)
+    axs[0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axs[0].tick_params(labelsize=13)
+    axs[1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axs[1].tick_params(labelsize=13)
+    axs[2].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axs[2].tick_params(labelsize=13)
 
-plt.subplots_adjust(hspace=0.1,wspace=0.1)
+plt.subplots_adjust(hspace=0.1,wspace=0.05)
 plt.savefig(work_out+'H158_meanshear_measured_properties_perbin.pdf')
