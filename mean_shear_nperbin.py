e703ff42b202a6aa0e73028fbd39681a65da7df4
Masaya Yamamoto <my137@duke.edu>
2021-10-20 20:12:29 -0400

    bug

--- mean_shear_nperbin.py
+++ mean_shear_nperbin.py
@@ -19,7 +19,6 @@ def mean_shear_nperbin(new, new1p, new1m, new2p, new2m, nperbin, par):
     else:
         coadd=False
     x_ = new[par]
-    print(par, len(x_), x_)
     hist = stat.histogram(x_, nperbin=nperbin, more=True)
     bin_num = len(hist['hist'])
     g_obs = np.zeros(bin_num)
@@ -35,19 +34,21 @@ def mean_shear_nperbin(new, new1p, new1m, new2p, new2m, nperbin, par):
 
 start = 0
 sets = ['g1002', 'g1n002', 'g2002', 'g2n002']
-fig,axs = plt.subplots(1,3,figsize=(20,6),dpi=100,sharey=True)
-for p,j in enumerate([coadd_path, single_path]):
+fig,axs = plt.subplots(1,4,figsize=(28,6),dpi=100,sharey=True)
+for p in enumerate(['coadd', 'single']):
     noshear = []
     shear1p = []
     shear1m = []
     shear2p = []
     shear2m = []
-    if p==0:
-        xax = ['coadd_snr', 'coadd_hlr', 'coadd_psf_T']
+    if p=='coadd':
+        xax = ['coadd_snr', 'coadd_hlr', 'size', 'coadd_psf_T']
         coadd=True
-    if p==1:
-        xax = ['snr', 'hlr', 'psf_T']
+        j = coadd_path
+    if p=='single':
+        xax = ['snr', 'hlr', 'size', 'psf_T']
         coadd=False
+        j = single_path
     for i in range(4): # four sets of sim. 
         mcal_noshear = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+j+"/fiducial_H158_mcal_noshear.fits"))[-1].read()
         mcal_1p = fio.FITS(os.path.join(work, sets[i]+"/ngmix/"+j+"/fiducial_H158_mcal_1p.fits"))[-1].read()
@@ -73,10 +74,10 @@ for p,j in enumerate([coadd_path, single_path]):
         new2p = shear2p[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
         new2m = shear2m[run][np.isin(noshear[run]['ind'] ,tmp_ind)]
 
-        print(new['snr'])
         bin_mean_snr, g1_obs_snr, g1err_obs_snr = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[0])
         bin_mean_T, g1_obs_T, g1err_obs_T = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[1])
-        bin_mean_Tpsf, g1_obs_Tpsf, g1err_obs_Tpsf = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[2])
+        bin_mean_size, g1_obs_size, g1err_obs_size = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[2])
+        bin_mean_Tpsf, g1_obs_Tpsf, g1err_obs_Tpsf = mean_shear_nperbin(new, new1p, new1m, new2p, new2m, 50000, xax[3])
 
         gamma1_t,gamma2_t,gamma1_o,gamma2_o,noshear1,noshear2 = analyze_gamma_obs(new,new1p,new1m,new2p,new2m,coadd_=coadd)
 
@@ -105,16 +106,20 @@ for p,j in enumerate([coadd_path, single_path]):
 
     axs[0].hlines(0.02, 0, bin_mean_snr[len(bin_mean_snr)-1],linestyles='dashed')
     axs[1].hlines(0.02, 0, bin_mean_T[len(bin_mean_T)-1],linestyles='dashed')
-    axs[2].hlines(0.02, 0, bin_mean_Tpsf[len(bin_mean_Tpsf)-1],linestyles='dashed')
-    axs[0].errorbar(bin_mean_snr, g1_obs_snr, yerr=g1err_obs_snr, fmt='o', fillstyle='none')
-    axs[1].errorbar(bin_mean_T, g1_obs_T, yerr=g1err_obs_T, fmt='o', fillstyle='none')
-    axs[2].errorbar(bin_mean_Tpsf, g1_obs_Tpsf, yerr=g1err_obs_Tpsf, fmt='o', fillstyle='none')
+    axs[2].hlines(0.02, 0, bin_mean_size[len(bin_mean_size)-1],linestyles='dashed')
+    axs[3].hlines(0.02, 0, bin_mean_Tpsf[len(bin_mean_Tpsf)-1],linestyles='dashed')
+    axs[0].errorbar(bin_mean_snr, g1_obs_snr, yerr=g1err_obs_snr, fmt='o', fillstyle='none', label=p)
+    axs[1].errorbar(bin_mean_T, g1_obs_T, yerr=g1err_obs_T, fmt='o', fillstyle='none', label=p)
+    axs[2].errorbar(bin_mean_size, g1_obs_size, yerr=g1err_obs_size, fmt='o', fillstyle='none', label=p)
+    axs[3].errorbar(bin_mean_Tpsf, g1_obs_Tpsf, yerr=g1err_obs_Tpsf, fmt='o', fillstyle='none', label=p)
     axs[0].set_xlabel('S/N', fontsize=15)
-    axs[1].set_xlabel('T', fontsize=15)
-    axs[2].set_xlabel(r'$T_{psf}$', fontsize=15)
+    axs[1].set_xlabel('Measured T', fontsize=15)
+    axs[2].set_xlabel('Truth T', fontsize=15)
+    axs[3].set_xlabel(r'$T_{psf}$', fontsize=15)
     axs[0].set_xscale('log')
     axs[1].set_xscale('log')
     axs[2].set_xscale('log')
+    axs[3].set_xscale('log')
     axs[0].set_ylabel(r'$<e_{1}>$', fontsize=15)
     axs[0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     axs[0].tick_params(labelsize=13)
@@ -122,6 +127,9 @@ for p,j in enumerate([coadd_path, single_path]):
     axs[1].tick_params(labelsize=13)
     axs[2].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     axs[2].tick_params(labelsize=13)
+    axs[3].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axs[3].tick_params(labelsize=13)
+    axs[3].legend(fontsize='large')
 
-plt.subplots_adjust(hspace=0.1,wspace=0.05)
+plt.subplots_adjust(hspace=0.1,wspace=0.02)
 plt.savefig(work_out+'H158_meanshear_measured_properties_perbin.pdf')
