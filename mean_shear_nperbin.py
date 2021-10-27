e203904dd3812819a4daa74f105123895b626a13
Masaya Yamamoto <my137@duke.edu>
2021-10-27 12:55:35 -0400

    bug

--- mean_shear_nperbin.py
+++ mean_shear_nperbin.py
@@ -13,11 +13,11 @@ sims = ['g1002', 'g1n002', 'g2002', 'g2n002']
 
 def hlr_to_T(d):
     # assuming galaxy profile is gaussian.
-    return 2*(2*d/(2.3548200450309493))**2
+    return 2*(2*d/2.3548200450309493)**2
 
 def mean_shear_nperbin(new, new1p, new1m, new2p, new2m, nperbin, par, coadd):
     
-    if 'hlr' in par:
+    if par=='size':
         x_ = hlr_to_T(new[par])
     else:
         x_ = new[par]
@@ -39,7 +39,7 @@ def mean_shear_nperbin(new, new1p, new1m, new2p, new2m, nperbin, par, coadd):
 start = 0
 sets = ['g1002', 'g1n002', 'g2002', 'g2n002']
 fig,axs = plt.subplots(2,4,figsize=(28,12),dpi=100,sharey=True)
-matplotlib.rcParams.update({'font.size': 22})
+matplotlib.rcParams.update({'font.size': 25})
 for p in ['coadd', 'single', 'multiband']:
     noshear = []
     shear1p = []
@@ -106,29 +106,6 @@ for p in ['coadd', 'single', 'multiband']:
 
     # gamma1_t,gamma2_t,gamma1_o,gamma2_o,noshear1,noshear2 = analyze_gamma_obs(new,new1p,new1m,new2p,new2m,coadd)
 
-    # d_x = [new['coadd_snr'], new['coadd_hlr'], new['coadd_psf_T']]
-    # x_label = ['S/N', 'T', 'T_{psf}']
-    # for q,ax in enumerate(axs.ravel()):
-
-    #     x_ = d_x[q]
-    #     hist = stat.histogram(x_, nperbin=50000, more=True)
-    #     bin_num = len(hist['hist'])
-    #     g_obs = np.zeros(bin_num)
-    #     gerr_obs = np.zeros(bin_num)
-    #     print(len(hist['low']), len(hist['mean']))
-    #     for i in range(bin_num):
-    #         bin_mask = (x_ > hist['low'][i]) & (x_ < hist['high'][i])
-    #         g_obs[i] = np.mean(gamma1_o[bin_mask])
-    #         gerr_obs[i] = np.std(gamma1_o[bin_mask])/np.sqrt(len(gamma1_o[bin_mask]))
-
-    #     ax.hlines(0.02, 0, hist['mean'][len(hist['mean'])-1],linestyles='dashed')
-    #     ax.errorbar(hist['mean'], g_obs, yerr=gerr_obs, fmt='o', fillstyle='none')
-    #     ax.set_xlabel(x_label[q])
-    #     ax.set_xscale('log')
-    #     ax.set_ylabel('<e1>')
-    #     ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-
-
     axs[0,0].hlines(0.00, 0, bin1_mean_snr[len(bin1_mean_snr)-1],linestyles='dashed')
     axs[0,0].errorbar(bin1_mean_snr, g1_obs_snr[0,:]-0.02, yerr=g1err_obs_snr[0,:], fmt='o', fillstyle='none', label=p)
     axs[0,0].set_xlabel('S/N')
@@ -187,9 +164,9 @@ for p in ['coadd', 'single', 'multiband']:
     axs[1,3].set_xscale('log')
     axs[1,3].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     axs[1,3].tick_params(labelsize=13)
-    axs[1,3].legend(fontsize='large')
+    axs[1,3].legend(loc=4)
 
 
 
-plt.subplots_adjust(hspace=0.1,wspace=0.02)
+plt.subplots_adjust(hspace=0.3,wspace=0.02)
 plt.savefig(work_out+'H158_meanshear_measured_properties_perbin.pdf')
