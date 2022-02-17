ca29cd25973c212515a02c58e7478d36e077d48b
Masaya Yamamoto <my137@duke.edu>
2022-02-17 10:04:56 -0500

    update

--- draw_figures_for_paper.py
+++ draw_figures_for_paper.py
@@ -429,9 +429,9 @@ def mcal_catalog_properties(filter_, coadd_, out_fname):
     # properties = np.zeros((len(mcal_noshear),5))
     single_band = False
     if not single_band:
-        columns = ['g1_true', 'g2_true', 'g1_obs', 'g2_obs', 'coadd_psf_e1', 'coadd_psf_e2', 'coadd_psf_T', 'coadd_snr', 'coadd_T', 'mag', 'size',  'int_e1', 'int_e2', 'e']
+        columns = ['g1_true', 'g2_true', 'g1_obs', 'g2_obs', 'coadd_psf_e1', 'coadd_psf_e2', 'coadd_psf_T', 'coadd_snr', 'coadd_T', 'mag', 'size',  'int_e1', 'int_e2', 'e1_true', 'e2_true', 'e']
     else:
-        columns = ['g1_true', 'g2_true', 'g1_obs', 'g2_obs', 'snr', 'hlr', 'mag', 'int_e1', 'int_e2', 'e'] # 'psf_e1', 'psf_e2', 'psf_T']
+        columns = ['g1_true', 'g2_true', 'g1_obs', 'g2_obs', 'snr', 'hlr', 'mag', 'int_e1', 'int_e2', 'e1_true', 'e2_true', 'e'] # 'psf_e1', 'psf_e2', 'psf_T']
 
     # properties[:,0] = mcal_noshear['coadd_snr']
     # properties[:,1] = mcal_noshear['coadd_hlr']
@@ -502,7 +502,9 @@ def mcal_catalog_properties(filter_, coadd_, out_fname):
             properties[start:start+total_obj, 6] = new['mag_'+filter_]
             properties[start:start+total_obj, 7] = new['int_e1']
             properties[start:start+total_obj, 8] = new['int_e2']
-            properties[start:start+total_obj, 9] = total_shape
+            properties[start:start+total_obj, 9] = shape1
+            properties[start:start+total_obj, 10] = shape2
+            properties[start:start+total_obj, 11] = total_shape
         else:
             gamma1_t,gamma2_t,gamma1_o,gamma2_o,noshear1,noshear2 = analyze_gamma_obs(new,new1p,new1m,new2p,new2m,coadd_=True)
             properties[start:start+total_obj, 0] = gamma1_t
@@ -518,7 +520,9 @@ def mcal_catalog_properties(filter_, coadd_, out_fname):
             properties[start:start+total_obj, 10] = new['size']
             properties[start:start+total_obj, 11] = new['int_e1']
             properties[start:start+total_obj, 12] = new['int_e2']
-            properties[start:start+total_obj, 13] = total_shape
+            properties[start:start+total_obj, 13] = shape1
+            properties[start:start+total_obj, 14] = shape2
+            properties[start:start+total_obj, 15] = total_shape
 
         start += total_obj
 
