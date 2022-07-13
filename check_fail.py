c895a4a7b73bedebc5c5070865bc0e78852e2fa6
Masaya Yamamoto <my137@duke.edu>
2022-07-13 10:00:28 -0400

    check failed ones

--- check_fail.py
+++ check_fail.py
@@ -2,12 +2,14 @@ import glob
 import numpy as np
 import fitsio as fio
 
-fs = glob.glob('/hpc/group/cosmology/phy-lsst/my137/roman_H158/g1002/ngmix/new_coadd_no_oversampling_psf/fiducial_H158_*_0_mcal_coadd_noshear.fits')
-flag_fail = 0
-total = 0
-for f in fs:
-    d = fio.read(f)
-    total += len(d)
-    nonzero_flag = d[d['coadd_e1']==0]
-    flag_fail += len(nonzero_flag)
-print(flag_fail, total)
+mcal_keys = ['noshear', '1p', '1m', '2p', '2m']
+for k in mcal_keys:
+    fs = glob.glob('/hpc/group/cosmology/phy-lsst/my137/roman_H158/g1002/ngmix/new_coadd_no_oversampling_psf/fiducial_H158_*_0_mcal_coadd_'+k+'.fits')
+    flag_fail = 0
+    total = 0
+    for f in fs:
+        d = fio.read(f)
+        total += len(d)
+        nonzero_flag = d[d['coadd_e1']==0]
+        flag_fail += len(nonzero_flag)
+    print(flag_fail, total)
