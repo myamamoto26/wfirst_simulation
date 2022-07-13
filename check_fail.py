248064fa1ba1ed5abcd9ff02c43af13d49edb19c
Masaya Yamamoto <my137@duke.edu>
2022-07-13 09:23:40 -0400

    check failed ones

---
+++ check_fail.py
@@ -0,0 +1,13 @@
+import glob
+import numpy as np
+import fitsio as fio
+
+fs = glob.glob('/hpc/group/cosmology/phy-lsst/my137/roman_H158/g1002/ngmix/new_coadd_no_oversampling_psf/fiducial_H158_*_0_mcal_coadd_noshear.fits')
+flag_fail = 0
+total = 0
+for f in fs:
+    d = fio.read(f)
+    total += len(d)
+    nonzero_flag = (d['flags']!=0)
+    flag_fail += len(nonzero_flag)
+print(flag_fail, total)
